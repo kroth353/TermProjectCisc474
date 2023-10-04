76554ab15342e13945b7d656e1d02c05aa602617
jthensley03 <jasonh@udel.edu>
2023-10-03 22:56:23 -0400

    names and ids for each card finished

--- js/game1.js
+++ js/game1.js
@@ -3,7 +3,7 @@ let animals = ["Alligator", "Cow", "Giraffe", "Koala", "Otter", "Rabbit", "Wolf"
 let colors = ["Brown", "ForestGreen", "Lavender", "Magenta", "Mint", "Teal", "babyPink", "blueGray", "brightGreen", "brightOrange", "brightRed", "brightYellow", "burntOrange", "darkBlue", "darkPurple", "deepRed", "gray", "hotPink", "lightBlue", "lightBrown", "lightGreen", "lightPink", "maroon", "mediumBlue", "mediumOrange", "middleYellow", "paleOrange", "paleYellow", "peach", "royalPurple", "skyBlue", "turquoise"];
 let numbers = ["1", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "2", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "3", "30", "31", "32", "4", "5", "6", "7", "8", "9"];
 var matchStr = "";
-var imgOption = 2;
+var imgOption = 1;
 
 function createCards() {
     console.log("creating " + numCards + " cards");
@@ -16,25 +16,24 @@ function createCards() {
     let cardWidth = cardWidthNum.toString();
     console.log("card width: " + cardWidth);
     let cardHeight = cardHeightNum.toString();
-
+    var imgs = [];
     if(imgOption == 1) {
-        let imgs = animals;
+        imgs = animals;
     } else if (imgOption == 2) {
-        let imgs = colors;
+        imgs = colors;
     } else {
-        let imgs = numbers;
+        imgs = numbers;
     }
     htmlStr = '';
     for(let i = 0; i<(numCards/2); i++) {
         cardStr = imgs[i];
-        htmlStr += "<div id='" + cardStr + "1' class='item-card' style='width: " + cardWidth + "px; height: " + cardHeight + "px;'><img class='front-face' src='img/" + cardStr + ".png'><img class='back-face' src='img/card.png'></div>" + "<div id='" + cardStr + "2' class='item-card' style='width: " + cardWidth + "px; height: " + cardHeight + "px;'><img class='front-face' src='img/" + cardStr + ".png'><img class='back-face' src='img/card.png'></div>";
-    htmlStr = '';
-    for(let i = 0; i<numCards; i++) {
-        htmlStr += "<div id='card" + i + "' class='item-card' style='width: " + cardWidth + "px; height: " + cardHeight + "px;'><img class='front-face' src='img/herc.svg'><img class='back-face' src='img/herc.svg'></div>";
+        htmlStr += "<div id='" + cardStr + "1' class='item-card' style='width: " + cardWidth + "px; height: " + cardHeight + "px;'><img class='front-face' src='images/" + cardStr + ".png'><img class='back-face' src='images/card.png'></div>" + "<div id='" + cardStr + "2' class='item-card' style='width: " + cardWidth + "px; height: " + cardHeight + "px;'><img class='front-face' src='images/" + cardStr + ".png'><img class='back-face' src='images/card.png'></div>";
     }
     document.getElementById('memory-game').innerHTML = htmlStr;
-    for(let j = 0; j<numCards; j++) {
-        document.getElementById("card"+j).addEventListener('click', flipCard);
+    for(let j = 0; j<(numCards/2); j++) {
+        cardStr = imgs[j];
+        document.getElementById(cardStr+"1").addEventListener('click', flipCard);
+        document.getElementById(cardStr+"2").addEventListener('click', flipCard);
     }
 }
 
@@ -47,13 +46,13 @@ async function startGame() {
     console.log("game started");
     hideStartButton();
     createCards();
-    flipCard();
-    await new Promise(r => setTimeout(r, 2000));
-    flipCard();
+//    flipCard();
+//    await new Promise(r => setTimeout(r, 2000));
+//    flipCard();
 }
 
 function flipCard() {
-    console.log("do a flip");
+    console.log("flip: " + this.id);
     this.classList.toggle('flip');
 }
 
