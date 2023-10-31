e79b972e7b99b5cfb042e643d95811cc5ba3ef33
RGUD0 <114261602+RGUD0@users.noreply.github.com>
2023-10-31 09:13:33 -0400

    Improved Game 2 Settings

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-/* Game 2 Settings */
-
-dimslider() {
-    var slider = document.getElementById("myRange");
-    var output = document.getElementById("demo");
-    output.innerHTML = slider.value;
-    output.innerHTML = this.value;
-}
-
